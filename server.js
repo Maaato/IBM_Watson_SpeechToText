e5d63bc30a05a9d9796d09bc9b5f872d2aa9410f
Maato <lagos.matias25@gmail.com>
2020-04-21 20:38:45 -0400

    v1.0

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static('./dist/speech-to-text-frontend'));
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/speech-to-text-frontend/" });
+});
+
+app.listen(process.env.PORT || 8080);
